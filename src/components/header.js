3bde160ceaf624829c85a5b3b5c235c094d5c6d3
Andrew Emeghebo <andrewemeghebo2023@gmail.com>
2024-05-28 20:25:37 -0400

    Just testing

--- src/components/header.js
+++ src/components/header.js
@@ -12,3 +12,17 @@ export function header() {
       </nav>
   `;
 }
+export function header() {
+  return `    
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Student Applications</li>
+          <li>Student Profiles</li>
+          <li>Accomplishments</li>
+          <li>Contact Us</li>
+        </ul>
+      </nav>
+  `;
+}
